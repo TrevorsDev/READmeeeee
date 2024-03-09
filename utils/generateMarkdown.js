ab57e0f6c680971ad7ab89511c700e021fdde4e3
Trevor Johnson <trevor9j@protonmail.com>
2024-03-09 16:49:32 -0700

    adding licenses

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,7 +4,28 @@ function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) {
+  // Extract the license type from the 'license' parameter
+  let licenseType = license.license;
+
+  // Initialize a variable to store the license badge string
+  let yourLicense = '';
+
+  // Check the license type and assign the corresponding badge string
+  if (licenseType === 'MIT') {
+    yourLicense = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
+  } else if (licenseType === 'GPLv3') {
+    yourLicense = `![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)`;
+  } else if (licenseType === 'GPL') {
+    yourLicense = `![GPL license](https://img.shields.io/badge/License-GPL-blue.svg)`;
+  } else {
+    // If the license type is not recognized, set it to "N/A"
+    license.license = "N/A";
+  }
+
+  // Return the license badge string
+  return yourLicense;
+} 
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
