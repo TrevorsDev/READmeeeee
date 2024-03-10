d0f664500e449655b72da30ea7352bf5d3429364
Trevor Johnson <trevor9j@protonmail.com>
2024-03-09 20:31:20 -0700

    adding license function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -25,7 +25,7 @@ function renderLicenseLink(license) {
 
   // Return the license badge string
   return yourLicense;
-} 
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
